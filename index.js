621d13cafc7fb10c232db0c3126d977950ea1b55
george kinyanjui1 <george.kinyajui@student.moringaschool.com>
2022-06-11 18:45:14 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+
+const superbowlWin = (record) => {
+    let myResult = record.find((item) => item.result === "W");
+    if (myResult) {
+        return myResult.year;
+    } else {
+        return undefined;
+    }
+}
